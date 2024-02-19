6cf0e5c1a44167d8d6c67112256cdacd05a06d9b
gallejens <galle.jens@telenet.be>
2024-02-19 17:23:54 +0100

    feat(frontend): add client router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
+import { RouterProvider } from '@tanstack/react-router';
 import type { FC } from 'react';
-import { TestValuesPage } from './pages/testvaluespage';
+import { router } from './router';
+
+// TODO: Add navbar here and use the router to show the current page while always showing the navbar
+// Also place overlays here (notifs, modals etc)
 
 export const App: FC = () => {
-  return <TestValuesPage />;
+  return <RouterProvider router={router} />;
 };

539e5d4587b5315e848bb5c249736a6620560480
MaximilianDuda <maximiliandudajunior@gmail.com>
2024-02-24 14:47:43 +0100

    Add bussines logic to NotificationService

--- backend/src/main/java/com/ap/steelduxxklantenportaal/services/NotificationService.java
+++ backend/src/main/java/com/ap/steelduxxklantenportaal/services/NotificationService.java
@@ -1,5 +1,25 @@
 package com.ap.steelduxxklantenportaal.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import com.ap.steelduxxklantenportaal.repositories.NotificationRepository;
+import com.ap.steelduxxklantenportaal.models.Notification;
+
+import java.util.List;
+
 public class NotificationService {
-    
+
+    private final NotificationRepository notificationRepository;
+
+    @Autowired
+    public NotificationService(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    public Notification createNotification(Notification notification) {
+        return notificationRepository.save(notification);
+    }
+
+    public List<Notification> getNotificationsByUserId(Long userId) {
+        return notificationRepository.findByUserId(userId);
+    }
 }

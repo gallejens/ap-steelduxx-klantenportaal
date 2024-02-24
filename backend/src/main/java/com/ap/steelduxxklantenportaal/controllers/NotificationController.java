6448363ffecba8e98e5a6d51debe3e88ac3221b8
MaximilianDuda <maximiliandudajunior@gmail.com>
2024-02-24 17:07:58 +0100

    Setup NotificationController

--- backend/src/main/java/com/ap/steelduxxklantenportaal/controllers/NotificationController.java
+++ backend/src/main/java/com/ap/steelduxxklantenportaal/controllers/NotificationController.java
@@ -1,5 +1,30 @@
 package com.ap.steelduxxklantenportaal.controllers;
 
+import com.ap.steelduxxklantenportaal.services.NotificationService;
+import com.ap.steelduxxklantenportaal.models.Notification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notification")
 public class NotificationController {
     
+    private final NotificationService notificationService;
+
+    @Autowired
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @PostMapping
+    public Notification createNotification(@RequestBody Notification notification) {
+        return notificationService.createNotification(notification);
+    }
+
+    @GetMapping("/{userId}")
+    public List<Notification> getNotificationsByUserId(@PathVariable Long userId) {
+        return notificationService.getNotificationsByUserId(userId);
+    }
 }

c41d954fad42317bd56ce49ac519ec2619fb941c
MaximilianDuda <maximiliandudajunior@gmail.com>
2024-02-24 14:35:19 +0100

    Add variables to Notification model

--- backend/src/main/java/com/ap/steelduxxklantenportaal/models/Notification.java
+++ backend/src/main/java/com/ap/steelduxxklantenportaal/models/Notification.java
@@ -1,5 +1,63 @@
 package com.ap.steelduxxklantenportaal.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.util.Date;
+
+@Entity
 public class Notification {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId; // om users te filteren(voor later)
+
+    private String message;
+
+    private Date CreatedAt;
+
+    public Notification() {
+    }
+
+    public Notification(Long id, Long userId, String message, Date CreatedAt) {
+        this.userId = userId;
+        this.message = message;
+        this.CreatedAt = CreatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getCreatedAt() {
+        return CreatedAt;
+    }
+
+    public void setCreatedAt(Date CreatedAt) {
+        this.CreatedAt = CreatedAt;
+    }
+
 }

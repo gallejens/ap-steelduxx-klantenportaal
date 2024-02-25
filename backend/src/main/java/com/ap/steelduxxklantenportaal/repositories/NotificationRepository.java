55a2d9ce8a5fe4ac788b8f4e104c84bb91fd8526
MaximilianDuda <maximiliandudajunior@gmail.com>
2024-02-25 16:32:43 +0100

    Update NotificationRepo

--- backend/src/main/java/com/ap/steelduxxklantenportaal/repositories/NotificationRepository.java
+++ backend/src/main/java/com/ap/steelduxxklantenportaal/repositories/NotificationRepository.java
@@ -4,9 +4,6 @@ import com.ap.steelduxxklantenportaal.models.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByUserId(Long userId);
 }

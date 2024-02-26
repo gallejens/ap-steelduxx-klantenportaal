3b838d43693f8f398a6379c6c4932436c6269063
MaximilianDuda <maximiliandudajunior@gmail.com>
2024-02-26 17:41:06 +0100

    Add create NotificationTable to db_migrations

---
+++ backend/src/main/resources/db/migration/V1__create_notifications_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `notification` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(255) NOT NULL,
+    `message` VARCHAR(255) NOT NULL,
+    `user_id` INT NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);

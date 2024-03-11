13cf5f5d0419294ac56dff95e07f912e63c7abde
Vanhoegaerden Raf <s128965@ap.be>
2024-03-11 22:32:56 +0100

    change(backend): code review replace autowired with constructor in controller class

--- backend/src/main/java/com/ap/steelduxxklantenportaal/controllers/UserRequestController.java
+++ backend/src/main/java/com/ap/steelduxxklantenportaal/controllers/UserRequestController.java
@@ -3,7 +3,6 @@ package com.ap.steelduxxklantenportaal.controllers;
 import com.ap.steelduxxklantenportaal.DTOs.UserRequestValuesDTO;
 import com.ap.steelduxxklantenportaal.services.UserRequestValueService;
 import jakarta.mail.MessagingException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +12,16 @@ import java.util.List;
 @RestController
 public class UserRequestController {
 
-    @Autowired
-    private UserRequestValueService userRequestValueService;
+    private final UserRequestValueService userRequestValueService;
+
+    public UserRequestController(UserRequestValueService userRequestValueService) {
+        this.userRequestValueService = userRequestValueService;
+    }
 
     @PostMapping("/user_request")
     @PreAuthorize("permitAll")
-    public ResponseEntity<Object> saveRequest(@RequestBody UserRequestValuesDTO userRequestValuesDTO) throws MessagingException {
+    public ResponseEntity<Object> saveRequest(@RequestBody UserRequestValuesDTO userRequestValuesDTO)
+            throws MessagingException {
         return userRequestValueService.processUserRequest(userRequestValuesDTO);
     }
 

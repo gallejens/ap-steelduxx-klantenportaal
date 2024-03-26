1142628841d0fb2c7a7ea7c655357da274368a64
Bayram Han Capa <bayramcapa85@gmail.com>
2024-03-26 21:41:43 +0100

    add(notificationpopover) refetch interval

--- frontend/src/components/notificationpopover/components/Notificationpopover.tsx
+++ frontend/src/components/notificationpopover/components/Notificationpopover.tsx
@@ -17,6 +17,7 @@ export function NotificationPopover() {
   } = useQuery({
     refetchOnWindowFocus: false,
     queryKey: ['notifications', user?.id],
+    refetchInterval: 2000,
     queryFn: () =>
       doApiAction<NotificationData[]>({
         endpoint: `notifications/user/new/${user?.id}`,

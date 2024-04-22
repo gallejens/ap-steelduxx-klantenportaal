200cc1dad91970c1e22539d8b3586ea6e580b61d
MaximilianDuda <maximiliandudajunior@gmail.com>
2024-04-22 22:49:37 +0200

    Add values to Dto's

--- backend/src/main/java/com/ap/steelduxxklantenportaal/dtos/ExternalAPI/OrderDto.java
+++ backend/src/main/java/com/ap/steelduxxklantenportaal/dtos/ExternalAPI/OrderDto.java
@@ -1,24 +1,27 @@
 package com.ap.steelduxxklantenportaal.dtos.ExternalAPI;
 
+import java.lang.reflect.Array;
+import java.util.List;
+
 import com.ap.steelduxxklantenportaal.enums.OrderStateEnum;
 import com.ap.steelduxxklantenportaal.enums.OrderTransportTypeEnum;
 
 public record OrderDto(
-        String customerCode,
-        String referenceNumber,
-        String customerReferenceNumber,
-        OrderStateEnum state,
-        OrderTransportTypeEnum transportType,
-        String portOfOriginCode,
-        String portOfOriginName,
-        String portOfDestinationCode,
-        String portOfDestinationName,
-        String shipName,
-        String ets,
-        String ats,
-        String eta,
-        String ata,
-        long totalWeight,
-        long totalContainers
-) {
+                String customerCode,
+                String referenceNumber,
+                String customerReferenceNumber,
+                OrderStateEnum state,
+                OrderTransportTypeEnum transportType,
+                String portOfOriginCode,
+                String portOfOriginName,
+                String portOfDestinationCode,
+                String portOfDestinationName,
+                String shipName,
+                String ets,
+                String ats,
+                String eta,
+                String ata,
+                long totalWeight,
+                long totalContainers,
+                List<String> containerTypes) {
 }

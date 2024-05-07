339c0c81773b19a8c3620969bff8eef795a1cf7a
gallejens <galle.jens@telenet.be>
2024-05-07 10:23:58 +0200

    test(company-info): add company info test

--- backend/src/test/java/com/ap/steelduxxklantenportaal/companyinfo/CompanyInfoObjectMother.java
+++ backend/src/test/java/com/ap/steelduxxklantenportaal/companyinfo/CompanyInfoObjectMother.java
@@ -1,17 +1,20 @@
-package com.ap.steelduxxklantenportaal.account;
+package com.ap.steelduxxklantenportaal.companyinfo;
 
-import com.ap.steelduxxklantenportaal.dtos.Accounts.CreateSubaccountDto;
+import com.ap.steelduxxklantenportaal.dtos.CompanyInfo.CreateSubAccountDto;
 import com.ap.steelduxxklantenportaal.enums.RoleEnum;
+import com.ap.steelduxxklantenportaal.models.Company;
 import com.ap.steelduxxklantenportaal.models.User;
 
 import java.util.UUID;
 
-public class AccountObjectMother {
+public class CompanyInfoObjectMother {
     static final String headAdminEmail = "admin@test.com";
     static final String headNormalEmail = "normal@test.com";
     static final User headAdminUser = createNewUser(headAdminEmail, "HeadAdmin", "Test", RoleEnum.ROLE_HEAD_ADMIN);
     static final User headNormalUser = createNewUser(headNormalEmail, "HeadNormal", "Test", RoleEnum.ROLE_HEAD_USER);
-    static final CreateSubaccountDto createSubaccountDto = new CreateSubaccountDto("new@test.com", "Test", "Test");
+    static final Company company = new Company(null, "", "", "", "", "", "", "", "", "", "", "");
+    static final String createAdminSubAccountEmail = "newadmin@test.com";
+    static final String createNormalSubAccountEmail = "newnormal@test.com";
 
     private static User createNewUser(String email, String firstName, String lastName, RoleEnum role) {
         var user = new User();

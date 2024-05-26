e87110cb0e3cd82cdfebb744e70c2886160f01b9
Vanhoegaerden Raf <s128965@ap.be>
2024-05-26 14:14:29 +0200

    fix(backend): remove findBy methods in repo

--- backend/src/main/java/com/ap/steelduxxklantenportaal/repositories/OrderRequestProductRepository.java
+++ backend/src/main/java/com/ap/steelduxxklantenportaal/repositories/OrderRequestProductRepository.java
@@ -1,7 +1,6 @@
 package com.ap.steelduxxklantenportaal.repositories;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +9,4 @@ import com.ap.steelduxxklantenportaal.models.OrderRequestProduct;
 @Repository
 public interface OrderRequestProductRepository extends JpaRepository<OrderRequestProduct, Long> {
     List<OrderRequestProduct> findAllByOrderRequestId(long orderRequestId);
-
-    Optional<OrderRequestProduct> findById(long orderRequestId);
 }
--- backend/src/main/java/com/ap/steelduxxklantenportaal/repositories/OrderRequestRepository.java
+++ backend/src/main/java/com/ap/steelduxxklantenportaal/repositories/OrderRequestRepository.java
@@ -1,6 +1,5 @@
 package com.ap.steelduxxklantenportaal.repositories;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +8,5 @@ import com.ap.steelduxxklantenportaal.models.OrderRequest;
 
 @Repository
 public interface OrderRequestRepository extends JpaRepository<OrderRequest, Long> {
-        Optional<OrderRequest> findById(long orderRequestId);
 
 }

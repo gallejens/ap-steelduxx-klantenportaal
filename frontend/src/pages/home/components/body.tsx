fa281807289166d1f44e68e64ca14f1a496c9170
gallejens <galle.jens@telenet.be>
2024-05-29 22:29:56 +0200

    fix(homepage): fix translations and card heights

--- frontend/src/pages/home/components/body.tsx
+++ frontend/src/pages/home/components/body.tsx
@@ -3,12 +3,13 @@ import { Text, Paper } from '@mantine/core';
 import styles from '../styles/home.module.scss';
 import { useAuth } from '@/hooks/useAuth';
 import { TABS } from '@/tabs';
-import { t } from 'i18next';
 import { useNavigate } from '@tanstack/react-router';
+import { useTranslation } from 'react-i18next';
 
 export const Body: FC = () => {
   const navigate = useNavigate();
   const { user } = useAuth();
+  const { t } = useTranslation();
 
   const visibleCards = useMemo(() => {
     return TABS.filter(
@@ -21,31 +22,33 @@ export const Body: FC = () => {
 
   return (
     <div className={styles.body}>
-      {visibleCards.map(card => (
-        <Paper
-          key={card.path}
-          className={styles.card}
-          onClick={() => {
-            navigate({
-              to: card.path as string,
-            });
-          }}
-        >
-          <div className={styles.icon}>
-            <card.icon
-              size={64}
-              color={`rgb(${card.color.r}, ${card.color.g}, ${card.color.b})`}
-            />
-          </div>
-          <Text>{t(`homePage:cards:${card.labelKey}:title`)}</Text>
-          <Text
-            size='sm'
-            c='dimmed'
+      <div className={styles.card_container}>
+        {visibleCards.map(card => (
+          <Paper
+            key={card.path}
+            className={styles.card}
+            onClick={() => {
+              navigate({
+                to: card.path as string,
+              });
+            }}
           >
-            {t(`homePage:cards:${card.labelKey}:description`)}
-          </Text>
-        </Paper>
-      ))}
+            <div className={styles.icon}>
+              <card.icon
+                size={64}
+                color={`rgb(${card.color.r}, ${card.color.g}, ${card.color.b})`}
+              />
+            </div>
+            <Text>{t(`homePage:cards:${card.labelKey}:title`)}</Text>
+            <Text
+              size='sm'
+              c='dimmed'
+            >
+              {t(`homePage:cards:${card.labelKey}:description`)}
+            </Text>
+          </Paper>
+        ))}
+      </div>
     </div>
   );
 };
--- frontend/src/pages/home/components/header.tsx
+++ frontend/src/pages/home/components/header.tsx
@@ -1,11 +1,12 @@
 import type { FC } from 'react';
 import { Title } from '@mantine/core';
-import { t } from 'i18next';
 import styles from '../styles/home.module.scss';
 import { useAuth } from '@/hooks/useAuth';
+import { useTranslation } from 'react-i18next';
 
 export const Header: FC = () => {
   const { user } = useAuth();
+  const { t } = useTranslation();
 
   return (
     <div className={styles.header}>
